ca0ab0df50c23b9ec094f9afa72146c090d745fa
faisalmujawar148 <faisal.mujawar4@gmail.com>
2024-08-14 12:31:34 +0100

    need to implement firebase

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Flash Card",
+  title: "Flashback",
   description: "A Flash Card Application to revise important things",
 };
 
